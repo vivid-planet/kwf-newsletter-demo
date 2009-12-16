52100d619855c1141bd7fb9bd7fe431f7a3af6a8
Niko Sams <ns@vivid-planet.com>
2009-12-16 10:17:37 +0000

    bessere standard acl; trl

--- bootstrap.php
+++ bootstrap.php
@@ -21,21 +21,18 @@ $front = Vps_Controller_Front_Component::getInstance();
 $acl = new Vps_Acl_Component();
 
 $acl->add(new Vps_Acl_Resource_MenuDropdown('settings',
-            array('text'=>'Einstellungen', 'icon'=>'wrench.png')));
+            array('text'=>trl('Einstellungen'), 'icon'=>'wrench.png')));
     $acl->add(new Vps_Acl_Resource_MenuUrl('vps_user_users',
-            array('text'=>'Benutzerverwaltung', 'icon'=>'user.png'),
+            array('text'=>trl('Benutzerverwaltung'), 'icon'=>'user.png'),
             '/vps/user/users'), 'settings');
         $acl->add(new Zend_Acl_Resource('vps_user_user'), 'vps_user_users');
     $acl->add(new Vps_Acl_Resource_MenuUrl('vps_pool_pools',
-            array('text'=>'Pools', 'icon'=>'book_open.png'),
+            array('text'=>trl('Pools'), 'icon'=>'book_open.png'),
             '/vps/pool/pools'), 'settings');
         $acl->add(new Zend_Acl_Resource('vps_pool_pool'), 'vps_pool_pools');
 
 
-$acl->allow('admin', 'vps_user_changeuser');
-$acl->allow('admin', 'vps_user_users');
-$acl->allow('admin', 'settings');
-$acl->allow('superuser', 'vps_user_users');
+$acl->allow('admin', null);
 $acl->allow('superuser', 'settings');
 Zend_Registry::set('acl', $acl);
 
