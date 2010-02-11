0606dbc909098c3f955134f7a176c9bfd5e1a192
Niko Sams <ns@vivid-planet.com>
2010-02-11 09:29:21 +0000

    paragrapsh entfernt, dafür über config aus und einschalten

--- Vpc/Template/Root/Component.php
+++ Vpc/Template/Root/Component.php
@@ -5,12 +5,10 @@ class Vpc_Template_Root_Component extends Vpc_Root_Component
     {
         $ret = parent::getSettings();
 
-        $ret['generators']['category']['component'] = 'Vpc_Template_Root_Category_Component';
-
         $ret['generators']['box']['component']['mainMenu'] = 'Vpc_Template_Menu_Main_Component';
         $ret['generators']['box']['component']['subMenu'] = 'Vpc_Template_Menu_Sub_Component';
         $ret['generators']['box']['component']['bottomMenu'] = 'Vpc_Template_Menu_Bottom_Component';
-        
+
         $ret['editComponents'] = array();
         return $ret;
     }
