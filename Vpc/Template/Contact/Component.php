9a79f5e3680def8e346d4ed34b9b5712172d9365
Christoph Eder <ce@vivid-planet.com>
2010-02-22 14:51:11 +0000

    - forumlar in formular geaendert

--- Vpc/Template/Contact/Component.php
+++ Vpc/Template/Contact/Component.php
@@ -4,7 +4,7 @@ class Vpc_Template_Contact_Component extends Vpc_Form_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['componentName'] = trl('Kontaktforumlar');
+        $ret['componentName'] = trl('Kontaktformular');
         $ret['placeholder']['submitButton'] = trl('Nachricht absenden');
         return $ret;
     }
