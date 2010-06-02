631791344e4b9273464f7be7191b8851c079d7b5
Stephan Weinhold <sw@vivid-planet.com>
2010-06-02 14:46:13 +0200

    niko: title ist in einer eigenen box

--- Vpc/Template/Root/Component.php
+++ Vpc/Template/Root/Component.php
@@ -8,8 +8,8 @@ class Vpc_Template_Root_Component extends Vpc_Root_Component
         $ret['generators']['box']['component']['mainMenu'] = 'Vpc_Template_Menu_Main_Component';
         $ret['generators']['box']['component']['subMenu'] = 'Vpc_Template_Menu_Sub_Component';
         $ret['generators']['box']['component']['bottomMenu'] = 'Vpc_Template_Menu_Bottom_Component';
-        $ret['generators']['box']['component']['title'] = 'Vpc_Box_TitleEditable_Component';
         $ret['generators']['box']['component']['metaTags'] = 'Vpc_Box_MetaTagsContent_Component';
+        $ret['generators']['title']['component'] = 'Vpc_Box_TitleEditable_Component';
 
         $ret['editComponents'] = array('title', 'metaTags');
         return $ret;
