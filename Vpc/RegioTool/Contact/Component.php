c945c1aab536ad4449c679054754c339d4e1bc4a
Christoph Eder <ce@vivid-planet.com>
2010-10-12 14:31:01 +0200

    - stylingaenderungen

--- Vpc/RegioTool/Contact/Component.php
+++ Vpc/RegioTool/Contact/Component.php
@@ -5,7 +5,7 @@ class Vpc_RegioTool_Contact_Component extends Vpc_Form_Component
     {
         $ret = parent::getSettings();
         $ret['componentName'] = trl('Kontaktformular');
-        $ret['placeholder']['submitButton'] = trlStatic('Send Message');
+        $ret['placeholder']['submitButton'] = trlStatic('Abschicken');
         return $ret;
     }
 }
--- Vpc/RegioTool/List/Gallery/Component.php
+++ Vpc/RegioTool/List/Gallery/Component.php
@@ -5,6 +5,7 @@ class Vpc_RegioTool_List_Gallery_Component extends Vpc_List_Gallery_Component
     {
         $ret = parent::getSettings();
         $contentWidth = 800;
+        $contentWidthWithSub = 575;
         $ret['dimensions'] = array(
             '1fullWidth'=>array(
                 'text' => trl('Ein Bild, volle Breite'),
@@ -38,6 +39,41 @@ class Vpc_RegioTool_List_Gallery_Component extends Vpc_List_Gallery_Component
                 'text' => trl('Fünf Bilder, volle Breite'),
                 'width' => floor(($contentWidth - 40) / 5),
                 'height' => floor(($contentWidth - 40) / 5),
+                'scale' => Vps_Media_Image::SCALE_CROP,
+                'imagesPerLine' => 5
+            ),
+            '1halfWidth'=>array(
+                'text' => trl('Ein Bild, mit Submenü'),
+                'width' => $contentWidthWithSub-2,
+                'height' => $contentWidthWithSub-2,
+                'scale' => Vps_Media_Image::SCALE_CROP,
+                'imagesPerLine' => 1
+            ),
+            '2halfWidth'=>array(
+                'text' => trl('Zwei Bilder, mit Submenü'),
+                'width' => floor(($contentWidthWithSub - 14) / 2),
+                'height' => floor((($contentWidthWithSub - 14) / 2)/1.5),
+                'scale' => Vps_Media_Image::SCALE_CROP,
+                'imagesPerLine' => 2
+            ),
+            '3halfWidth'=>array(
+                'text' => trl('Drei Bilder, mit Submenü'),
+                'width' => floor(($contentWidthWithSub - 26) / 3),
+                'height' => floor((($contentWidthWithSub - 26) / 3)/1.5),
+                'scale' => Vps_Media_Image::SCALE_CROP,
+                'imagesPerLine' => 3
+            ),
+            '4halfWidth'=>array(
+                'text' => trl('Vier Bilder, mit Submenü'),
+                'width' => floor(($contentWidthWithSub - 38) / 4),
+                'height' => floor((($contentWidthWithSub - 38) / 4)/1.5),
+                'scale' => Vps_Media_Image::SCALE_CROP,
+                'imagesPerLine' => 4
+            ),
+            '5halfWidth'=>array(
+                'text' => trl('Fünf Bilder, mit Submenü'),
+                'width' => floor(($contentWidthWithSub - 50) / 5),
+                'height' => floor((($contentWidthWithSub - 50) / 5)/1.5),
                 'scale' => Vps_Media_Image::SCALE_CROP,
                 'imagesPerLine' => 5
             )
