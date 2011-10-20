c416c6d396d718c52af2487010fe8210bb4fafc9
Niko Sams <ns@vivid-planet.com>
2011-10-20 09:45:52 +0200

    trl

--- Vpc/Koala/Contact/Component.php
+++ Vpc/Koala/Contact/Component.php
@@ -4,8 +4,8 @@ class Vpc_Koala_Contact_Component extends Vpc_Form_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['componentName'] = trl('Kontaktformular');
-        $ret['placeholder']['submitButton'] = trlStatic('Abschicken');
+        $ret['componentName'] = trl('Contactform');
+        $ret['placeholder']['submitButton'] = trlStatic('Submit');
         return $ret;
     }
 }
